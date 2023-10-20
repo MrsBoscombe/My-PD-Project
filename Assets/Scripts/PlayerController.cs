52965604986fc08b51f2d033133e6a95ab0f1924
Mrs. Boscombe <30632430+MrsBoscombe@users.noreply.github.com>
2023-10-20 18:58:57 -0400

    Almost through week 5
    Added collectibles and UI.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,20 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using TMPro;
 
 
 public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
+    private int count;
     private float movementX;
     private float movementY;
     // Why not just create a Vector3 here?
     private Vector3 movementVector3;
     public float speed = 0;
+    private int numCollectibles;
+
+    [SerializeField] private TextMeshProUGUI countText;
+    [SerializeField] private GameObject winTextObject;
     // Start is called before the first frame update
     void Start()
     {
+        winTextObject.SetActive(false);
         rb = GetComponent<Rigidbody>();
+        count = 0;
+        numCollectibles = 12;       // change if the number of collectibles changes!
+        SetCountText();
     }
 
     void OnMove(InputValue movementValue){
@@ -33,6 +43,21 @@ public class PlayerController : MonoBehaviour
         rb.AddForce(movementVector3 * speed);    
 
 
+    }
+
+    void OnTriggerEnter(Collider other){
+        if (other.gameObject.CompareTag("Pickup")){
+            other.gameObject.SetActive(false);
+            count++;
+            SetCountText();
+        }
+    }
+
+    void SetCountText(){
+        countText.text = "Count: " + count.ToString();
+        if (count >= numCollectibles){
+            winTextObject.SetActive(true);
+        }
     }
     
 }
