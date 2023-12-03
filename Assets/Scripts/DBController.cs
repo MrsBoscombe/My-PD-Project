6f3940806d0e5f02c8fb7ae33ccd67ec563d570b
Mrs. Boscombe <30632430+MrsBoscombe@users.noreply.github.com>
2023-12-02 19:47:52 -0500

    Add DB character
    Add DB character and animations.

---
+++ Assets/Scripts/DBController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+    Animations were downloaded from Mixamo
+    12-2-2023
+
+*/
+
+public class DBController : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //To duplicate this code block, select the code between this comment
+        if (Input.GetKeyDown(KeyCode.M))
+        {
+            anim.SetTrigger("Magic");
+        }
+        else if (Input.GetKeyDown(KeyCode.Y))
+        {
+            anim.SetTrigger("Yawn");
+        }
+
+    }
+}
